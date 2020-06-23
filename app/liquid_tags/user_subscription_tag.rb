633e7c8bd1a71dc734063ba04eb5aed9608541c4
Alex <alexandersmith223@gmail.com>
2020-06-23 11:51:58 -0400

    MVP UX

--- app/liquid_tags/user_subscription_tag.rb
+++ app/liquid_tags/user_subscription_tag.rb
@@ -1,9 +1,18 @@
 class UserSubscriptionTag < LiquidTagBase
+  PARTIAL = "liquids/user_subscription".freeze
+
   def initialize(_tag_name, cta_text, _tokens)
     @cta_text = cta_text.strip
   end
 
-  def render(context); end
+  def render(_context)
+    ActionController::Base.new.render_to_string(
+      partial: PARTIAL,
+      locals: {
+        cta_text: @cta_text
+      },
+    )
+  end
 end
 
 Liquid::Template.register_tag("user_subscription", UserSubscriptionTag)
